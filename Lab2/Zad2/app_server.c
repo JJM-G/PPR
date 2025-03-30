6c9b501b81ade51bd57857e5c92dfd0bc3d6f7ea
G2Jackob <131303321+G2Jackob@users.noreply.github.com>
2025-03-30 17:53:46 +0200

    Update app_server.c

--- Lab2/Zad2/app_server.c
+++ Lab2/Zad2/app_server.c
@@ -3,13 +3,11 @@
 wyjscie * obliczenia_1_svc(wejscie *argp, struct svc_req *rqstp) {
     static wyjscie result;
 
-    // Wypisujemy natychmiast odebrane dane
+   
     printf("%s", argp->x1);
-    fflush(stdout);  // Zapewnia natychmiastowe wyświetlenie
-
-    // Kopiujemy dane do wyniku (ale bez gromadzenia ich na zawsze)
+    fflush(stdout);
     strncpy(result.suma, argp->x1, sizeof(result.suma) - 1);
-    result.suma[sizeof(result.suma) - 1] = '\0';  // Zapewniamy zakończenie stringa
+    result.suma[sizeof(result.suma) - 1] = '\0'; 
 
     return &result;
 }
